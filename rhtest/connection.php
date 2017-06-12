44ab55f883b06da90fb9923f2f7822e5ea160d5b
Davi Costa <davispcosta@gmail.com>
2017-06-12 01:07:56 -0300

    Atualizando

--- rhtest/connection.php
+++
@@ -1,7 +0,0 @@
-<?php
-$banco = "rhoutdoor";
-$senha = "p104prbk";
-$host = "mysql:host=mysql5"; 
-$dbhandle = mysql_connect($host, $banco, $senha);
-$db_selected = mysql_select_db('rhoutdoor', $dbhandle); 
-?>
